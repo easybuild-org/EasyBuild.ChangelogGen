8a56b2921fcc4bf2732f60752a5e8048333d87e8
Maxime Mangel <mangel.maxime@protonmail.com>
2024-11-17 23:20:25 +0100

    chore: setup EasyBuild process

---
+++ build/Commands/Release.fs
@@ -0,0 +1,56 @@
+module EasyBuild.Commands.Release
+
+open Spectre.Console.Cli
+open SimpleExec
+open EasyBuild.Workspace
+open System
+open System.IO
+open BlackFox.CommandLine
+open EasyBuild.Tools.Dotnet
+open EasyBuild.Commands.Test
+
+type ReleaseSettings() =
+    inherit CommandSettings()
+
+type ReleaseCommand() =
+    inherit Command<ReleaseSettings>()
+    interface ICommandLimiter<ReleaseSettings>
+
+    override __.Execute(context, settings) =
+        TestCommand().Execute(context, TestSettings()) |> ignore
+
+        // Clean up the src/bin folder
+        if Directory.Exists VirtualWorkspace.src.bin.``.`` then
+            Directory.Delete(VirtualWorkspace.src.bin.``.``, true)
+
+        let (struct (newVersion, _)) =
+            Command.ReadAsync(
+                "dotnet",
+                CmdLine.empty |> CmdLine.appendRaw "changelog-gen" |> CmdLine.toString,
+                workingDirectory = Workspace.``.``
+            )
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+
+        let nupkgPath = Nuget.pack Workspace.src.``.``
+
+        let nugetKey =
+            match Environment.GetEnvironmentVariable("NUGET_KEY") with
+            | null -> failwith "NUGET_KEY environment variable not set."
+            | key -> key
+
+        Nuget.push (nupkgPath, nugetKey)
+
+        Command.Run("git", "add .")
+
+        Command.Run(
+            "git",
+            CmdLine.empty
+            |> CmdLine.appendRaw "commit"
+            |> CmdLine.appendPrefix "-m" $"chore: release %s{newVersion}"
+            |> CmdLine.toString
+        )
+
+        Command.Run("git", "push")
+
+        0
