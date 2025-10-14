bc5f010b1531b1e4ced3a07de56d199e73ec93ff
Maxime Mangel <me@mangelmaxime.fr>
2025-10-14 22:19:58 +0200

    fix: fix logger to report the error and information correctly in the console

--- src/Log.fs
+++ src/Log.fs
@@ -9,15 +9,15 @@ let private output =
     AnsiConsole.Create(settings)
 
 let info msg =
-    output.MarkupLine($"[deepskyblue3_1]{0}[/]", Markup.Escape(msg))
+    output.MarkupLine("[deepskyblue3_1]{0}[/]", Markup.Escape(msg))
 
 let success msg =
-    output.MarkupLine($"[green]{0}[/]", Markup.Escape(msg))
+    output.MarkupLine("[green]{0}[/]", Markup.Escape(msg))
 
 let log msg = output.MarkupLine(msg)
 
 let error msg =
-    output.MarkupLine($"[red]{0}[/]", Markup.Escape(msg))
+    output.MarkupLine("[red]{0}[/]", Markup.Escape(msg))
 
 let warning msg =
-    output.MarkupLine($"[yellow]{0}[/]", Markup.Escape(msg))
+    output.MarkupLine("[yellow]{0}[/]", Markup.Escape(msg))
